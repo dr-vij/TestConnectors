9c313f0e3df9763f81cd32d443c8ca6ec65ae1fe
wh00ps <drspritz@icloud.com>
2022-03-24 19:29:20 +0300

    Реализация генератора объектов

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -4,5 +4,24 @@ using UnityEngine;
 
 public class Main : MonoBehaviour
 {
+    public const int OBJECTS_COUNT = 10;
+
+    public GameObject connectableOrigin;
     public float Radius = 10;
+
+    Transform m_transform;
+
+    private void Awake()
+    {
+        m_transform = transform;
+    }
+
+    private void Start()
+    {
+        for (int i = 0; i < OBJECTS_COUNT; i++)
+        {
+            Vector3 spotPosition = Quaternion.AngleAxis(360f * Random.value, Vector3.up) * Vector3.forward * Random.value * Radius;
+            Instantiate(connectableOrigin, spotPosition, Quaternion.identity, m_transform);
+        }
+    }
 }
