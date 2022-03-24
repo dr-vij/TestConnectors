fdf85228ddbf9bb3dce9489829131348c2a40cfa
wh00ps <drspritz@icloud.com>
2022-03-24 21:38:15 +0300

    Конфигурирование слоев объектов для работы коннектора

---
+++ Assets/Scripts/ConnectionController1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConnectionController1 : MonoBehaviour, IConnectionStatusProvider
+{
+    public event Action<GameObject> onStartConnection;
+    public event Action<GameObject> onCompleteConnection;   
+}
