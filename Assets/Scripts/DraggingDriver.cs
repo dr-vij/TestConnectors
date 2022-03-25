a14d97090fde4219d32856ef7684e21bdd4046a6
wh00ps <drspritz@icloud.com>
2022-03-25 10:29:30 +0300

    Улучшение трекинга перетягиваемого объекта

--- Assets/Scripts/DraggingDriver.cs
+++ Assets/Scripts/DraggingDriver.cs
@@ -8,6 +8,9 @@ public class DraggingDriver : MonoBehaviour
 
     Camera m_camera;
     Transform m_draggingTarget;
+    Transform m_draggingSurface;
+
+    Vector3 m_initDraggingSurfaceLocalPosition;
 
     private void Awake()
     {
@@ -15,10 +18,20 @@ public class DraggingDriver : MonoBehaviour
         m_draggingTarget = transform.parent;
     }
 
+    private void Start()
+    {
+        m_draggingSurface = GameObject.FindGameObjectWithTag("DraggingSurface").transform;
+        m_initDraggingSurfaceLocalPosition = m_draggingSurface.localPosition;
+    }
+
     Vector3 m_positionCorrection;
 
-    private void OnMouseDown()
+    private IEnumerator OnMouseDown()
     {
+        InitializeDraggingSurface();
+
+        yield return new WaitForFixedUpdate();
+
         Vector3 draggingSurfacceHitPoint;
         if (GetDraggingSurfaceHitPoint(out draggingSurfacceHitPoint))
             m_positionCorrection = m_draggingTarget.position - draggingSurfacceHitPoint;
@@ -29,6 +42,30 @@ public class DraggingDriver : MonoBehaviour
         Vector3 draggingSurfacceHitPoint;
         if (GetDraggingSurfaceHitPoint(out draggingSurfacceHitPoint))
             m_draggingTarget.position = draggingSurfacceHitPoint + m_positionCorrection;
+    }
+
+    private void OnMouseUp()
+    {
+        DeinitializeDraggingSurface();
+    }
+
+    void InitializeDraggingSurface()
+    {
+        RaycastHit hit;
+        Ray mouseScreenRay = m_camera.ScreenPointToRay(Input.mousePosition);
+        bool isHit = Physics.Raycast(mouseScreenRay, out hit, Mathf.Infinity, LayerMask.GetMask(LayerMask.LayerToName(gameObject.layer)));
+
+        if (isHit)
+            m_draggingSurface.position = hit.point;
+    }
+
+    void DeinitializeDraggingSurface()
+    {
+        if (m_draggingSurface)
+            return;
+
+        m_draggingSurface.localPosition = m_initDraggingSurfaceLocalPosition;
+        m_draggingSurface = null;
     }
 
     bool GetDraggingSurfaceHitPoint(out Vector3 point)
